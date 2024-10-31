a6d566b230f055e91c51bb93d4fc7ccc2f42b061
Leo <leomiasiro@gmail.com>
2024-10-31 00:19:49 -0300

    Project Complete

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   has_many :tasks, dependent: :destroy
 
+  validates :position, uniqueness: true, presence: true
   validates :name, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :list
 
+  validates :list_position, uniqueness: { scope: :list_id }, presence: true
   validates :name, presence: true
 end
+
